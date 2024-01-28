f5914e7a3162c65ea88e0c8eb737d23bb2a9522a
SatishKumar <53410112+Satish-KumarGupta@users.noreply.github.com>
2024-01-28 20:36:45 +0530

    Create navItem.tsx

---
+++ navItem.tsx
@@ -0,0 +1,19 @@
+import { NavLinkProps } from '@ms/variable/types/other';
+export const NavItems: NavLinkProps[] = [
+  {
+    label: 'Home',
+    path: '/'
+  },
+  {
+    label: 'Service Request',
+    path: '#'
+  },
+  {
+    label: 'Help',
+    path: '#'
+  },
+  {
+    label: 'Contact Us',
+    path: '/contact'
+  }
+];
